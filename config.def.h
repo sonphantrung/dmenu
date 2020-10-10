fb5c66c31d46ae7392df3878c983ee64d8adf9e1
sonphantrung <phantrungson17@gmail.com>
2020-10-10 13:56:45 +0700

    Edit

--- config.def.h
+++ config.def.h
@@ -2,9 +2,11 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono Nerd Font:size=10"
+	"JetBrainsMono Nerd Font:size=10",
         "JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -18,10 +20,13 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 30;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 10;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
 static const char worddelimiters[] = " ";
+
+/* Size of the window border */
+static const unsigned int border_width = 5;
