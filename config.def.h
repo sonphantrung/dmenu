9e6776ad619c302d4e78ba80bed434142ab63208
Son Phan Trung <phantrungson17@gmail.com>
2021-08-20 14:09:10 +0700

    Added Xres support

--- config.def.h
+++ config.def.h
@@ -9,21 +9,33 @@ static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10",
-	"Noto Color Emoji:pixelsize=8:antialias=true:autohint=true"
-,
+	"Noto Color Emoji:pixelsize=8:antialias=true:autohint=true",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
+
+static char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static char normfgcolor[] = "#dae1ea";
+static char normbgcolor[] = "#151515";
+static char selfgcolor[]  = "#dae1ea";
+static char selbgcolor[]  = "#5e8d87";
+static char midfgcolor[]  = "#dae1ea";
+static char midbgcolor[]  = "#c13939";
+static char highlightnormfgcolor[]  = "#ffc978";
+static char highlightnormbgcolor[]  = "#222222";
+static char highlightselfgcolor[]  = "#ffc978";
+static char highlightselbgcolor[]  = "#5e8d87";
+
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d0ddd4", "#151515" },
-	[SchemeSel] = { "#dae1ea", "#5e8d87" },
-	[SchemeMid] = { "#dae1ea", "#c13939" },
-	[SchemeSelHighlight] = { "#ffc978", "#5e8d87" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNorm] = { normfgcolor, normbgcolor },
+	[SchemeSel] = { selfgcolor, selbgcolor },
+	[SchemeMid] = { midfgcolor, midbgcolor },
+	[SchemeSelHighlight] = { highlightselfgcolor, highlightselbgcolor },
+	[SchemeNormHighlight] = { highlightnormfgcolor, highlightnormbgcolor },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
-static const unsigned int alphas[SchemeLast][2] = {
+static unsigned int alphas[SchemeLast][2] = {
 	[SchemeNorm] = { OPAQUE, alpha },
 	[SchemeSel] = { OPAQUE, OPAQUE },
 	[SchemeMid] = { OPAQUE, OPAQUE },
@@ -36,6 +48,7 @@ static const unsigned int alphas[SchemeLast][2] = {
 static unsigned int lines      = 0;
 static unsigned int lineheight = 10;         /* -h option; minimum height of a menu line     */
 
+
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
@@ -43,3 +56,21 @@ static unsigned int lineheight = 10;         /* -h option; minimum height of a m
 static const char worddelimiters[] = " ";
 /* Size of the window border */
 static unsigned int border_width = 0;
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+	{ "color4", STRING, &normfgcolor },
+	{ "color0", STRING, &normbgcolor },
+	{ "color0",  STRING, &selfgcolor },
+	{ "color8",  STRING, &selbgcolor },
+	{ "foreground",  STRING, &midfgcolor },
+	{ "color1",  STRING, &midbgcolor },
+	{ "color3",  STRING, &highlightnormfgcolor },
+	{ "background",  STRING, &highlightnormbgcolor },
+	{ "color3",  STRING, &highlightselfgcolor },
+	{ "color6",  STRING, &highlightselbgcolor },
+	{ "prompt",      STRING, &prompt },
+	{ "border_width",      INTEGER, &border_width },
+};
